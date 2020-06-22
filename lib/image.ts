1e13b156424335ea1ec7024644fc326901c9bab1
Philipp Garbe <philipp.garbe@rio.cloud>
2020-06-22 09:41:55 +0200

    chore: Add awslint

--- lib/image.ts
+++ lib/image.ts
@@ -1,12 +1,18 @@
+/**
+ * Properties of a EcrSync image.
+ */
 export interface Image {
+
   /**
    * The name of the image that should be proxied by ECR
    *
    */
   readonly imageName: string;
+
   /**
    * Should the "latest" tag also included? Keep in mind that "latest" is not a version!
    *
+   * @default false
    */
   readonly includeLatest?: boolean;
 }
