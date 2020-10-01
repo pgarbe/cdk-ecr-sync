4025c9282d40d30d5b3dc62a826b7f51b20986e4
Philipp Garbe <philipp.garbe@rio.cloud>
2020-10-01 11:07:47 +0200

    Useful option to reduce number of tags Fixes #73

--- test/index.get-image-tags-handler.test.ts
+++ test/index.get-image-tags-handler.test.ts
@@ -1,4 +1,5 @@
 import * as handler from '../src/lambda/get-image-tags-handler';
+import { Image } from '../src/image';
 
 test('Docker image tags are loaded through pagination', async (done) => {
 
@@ -23,3 +24,52 @@ test('Docker library image tags are loaded through pagination', async (done) => 
   done();
 
 }, 30000);
+
+test('Latest images are ignored', async (done) => {
+
+  // WHEN
+  const dockerImageTags = ['latest'];
+  const ecrImageTags = [''];
+  const image: Image = { imageName: 'myImage', includeLatest: true };
+
+  let tags = await handler.filterTags(dockerImageTags, ecrImageTags, image);
+
+  // THEN
+  expect(tags.length).toBe(1);
+  expect(tags[0]).toBe('latest');
+
+  done();
+});
+
+test('Only included tags', async (done) => {
+
+  // WHEN
+  const dockerImageTags = ['1.0.0', '1.5', '2.0', 'latest'];
+  const ecrImageTags = [''];
+  const image: Image = { imageName: 'myImage', includeTags: '^1' };
+
+  let tags = await handler.filterTags(dockerImageTags, ecrImageTags, image);
+
+  // THEN
+  expect(tags.length).toBe(2);
+  expect(tags[0]).toBe('1.0.0');
+  expect(tags[1]).toBe('1.5');
+
+  done();
+});
+
+test('Exclude wins over include', async (done) => {
+
+  // WHEN
+  const dockerImageTags = ['1.0.0', '1.5', '2.0', 'latest'];
+  const ecrImageTags = [''];
+  const image: Image = { imageName: 'myImage', includeTags: '^1', excludeTags: '1.5' };
+
+  let tags = await handler.filterTags(dockerImageTags, ecrImageTags, image);
+
+  // THEN
+  expect(tags.length).toBe(1);
+  expect(tags[0]).toBe('1.0.0');
+
+  done();
+});
