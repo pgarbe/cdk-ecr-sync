cbaa500abfd5223204ff22f7f6a7f6d5bbea84a4
Philipp Garbe <git@garbe.io>
2022-10-04 15:52:28 +0200

    chore: upgrade deps

--- test/docker-adapter.test.ts
+++ test/docker-adapter.test.ts
@@ -3,7 +3,7 @@ import * as docker from '../src/lambda/docker-adapter';
 test('Docker image tags are loaded through pagination', async () => {
 
   // WHEN
-  let tags = await docker.getDockerImageTags('datadog/agent');
+  const tags = await docker.getDockerImageTags('datadog/agent');
 
   // THEN
   expect(tags.length).toBeGreaterThan(100);
@@ -15,10 +15,10 @@ test('Docker image tags are loaded through pagination', async () => {
 test('Docker library image tags are loaded through pagination', async () => {
 
   // WHEN
-  let tags = await docker.getDockerImageTags('amazonlinux');
+  const tags = await docker.getDockerImageTags('amazonlinux');
 
   // THEN
-  expect(tags.length).toBeGreaterThan(0);
+  expect(tags.length).toBeGreaterThan(100);
   expect(tags[0].digest?.length).toBeGreaterThan(0);
   expect(tags[0].tag.length).toBeGreaterThan(0);
 
@@ -28,7 +28,7 @@ test('Docker library image tags are loaded through pagination', async () => {
 test('Docker library image tags include only amd64/linux images', async () => {
 
   // WHEN
-  let tags = await docker.getDockerImageTags('mongo');
+  const tags = await docker.getDockerImageTags('mongo');
 
   // THEN
   expect(tags.filter(t => t.tag === '4.2.4').length).toBe(0); // mongo:4.2.4 has only amd64/windows images
